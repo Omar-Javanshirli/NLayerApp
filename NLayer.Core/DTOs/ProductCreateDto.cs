4dfe506ac64b8d34e9efe370fd7fdde40b5ca614
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-04-28 14:43:31 -0700

    new update

--- NLayer.Core/DTOs/ProductCreateDto.cs
+++ NLayer.Core/DTOs/ProductCreateDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NLayer.Core.DTOs
 {
-    public interface ProductCreateDto
+    public class ProductCreateDto
     {
         public string Name { get; set; }
         public int Stock { get; set; }
