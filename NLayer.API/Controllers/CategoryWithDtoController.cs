253bee9937e8c77bbce86baa46f8b8e7b9854cd6
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-04-28 16:37:27 -0700

    new update

---
+++ NLayer.API/Controllers/CategoryWithDtoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Model;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryWithDtoController : CustomBaseController
+    {
+        private readonly IServiceWithDto<Category, CategoryDTO> _service;
+
+        public CategoryWithDtoController(IServiceWithDto<Category, CategoryDTO> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return CreateActionResult(await _service.GetAllAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(CategoryDTO category)
+        {
+            return CreateActionResult(await _service.AddAsync(category));
+        }
+    }
+}
